05bc6fda6aabe28b4d6f93df9fae08860760d572
bowuting <bowuting@gmail.com>
2016-10-30 20:03:32 +0800

    数据库操作

--- tp3/Application/Home/Conf/config.php
+++ tp3/Application/Home/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'DB_TYPE'=>'mysql',// 数据库类型
-    'DB_HOST'=>'127.0.0.1',// 服务器地址
+    'DB_HOST'=>'localhost',// 服务器地址
     'DB_NAME'=>'thinkphp',// 数据库名
     'DB_USER'=>'root',// 用户名
     'DB_PWD'=>'root',// 密码
---
+++ tp3/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?> <!DOCTYPE html>
+ <html>
+     <head>
+         <meta charset="utf-8">
+         <title></title>
+     </head>
+     <body>
+         <h2><?php echo ($result["id"]); ?>--<?php echo ($result["data"]); ?></h2>
+     </body>
+ </html>
